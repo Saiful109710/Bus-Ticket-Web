c6ef4d0e9f4dfbc49aa549791504f945176da24d
saiful-islam <saifulislam109710@gmail.com>
2024-09-22 03:02:22 +0600

    start bus web by banner design

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        colors:{
+            btnColor:'rgba(27, 209, 0)'
+        }
+      },
+    },
+    plugins: [],
+  }
